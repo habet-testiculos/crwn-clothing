bc921ea99668ecb5d300b2bb91d57c49723cc5d8
safkanaba <faruk_arslan@hotmail.com.tr>
2021-05-29 21:44:25 +0300

    added shop page and collection preview component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div class="shop-page">
+                {
+                    collections.map(({ id, ...parameters }) => <CollectionPreview key={id} {...parameters} />)
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
