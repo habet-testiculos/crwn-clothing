5373b9327635b54620e863d079f81a2c12379196
safkanaba <faruk_arslan@hotmail.com.tr>
2021-05-30 14:36:22 +0300

    changeed name

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,7 +42,7 @@ class SignIn extends React.Component {
                     <FormInput label="password" name="password" type="password" value={this.state.password} required handleChange={this.handleChange} />
                     <div className="buttons">
                         <CustomButton type="submit">Submit Form</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
                     </div>
                 </form>
             </div>
